fc68ca24be6a00f59ee81fcb612df4c6e79e3e47
Rakshith <rakshi059@gmail.com>
2018-11-22 10:52:21 +0530

    changed the code allignment

--- app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
+++ app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
@@ -8,23 +8,29 @@ class MenuItemModel : Parcelable {
         this.sectionSlug = mSectionSlug
         this.mTitle = title
     }
+
     var sectionSlug: String?
     var mTitle: String?
+
     constructor(parcel: Parcel) {
         this.sectionSlug = parcel.readString()
         this.mTitle = parcel.readString()
     }
+
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(sectionSlug)
         parcel.writeString(mTitle)
     }
+
     override fun describeContents(): Int {
         return 0
     }
+
     companion object CREATOR : Parcelable.Creator<MenuItemModel> {
         override fun createFromParcel(parcel: Parcel): MenuItemModel {
             return MenuItemModel(parcel)
         }
+
         override fun newArray(size: Int): Array<MenuItemModel?> {
             return arrayOfNulls(size)
         }
