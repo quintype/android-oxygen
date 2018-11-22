55a36ca5b01702436811c8a33023efb5d3a07f57
Deepak <deepakwaitz@gmail.com>
2018-11-22 14:42:15 +0530

    Modified MenuItemModel

--- app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
+++ app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
@@ -7,6 +7,50 @@ import android.os.Parcelable
  * Created TemplateCollectionWithRx by rakshith on 10/8/18.
  */
 
+class MenuItemModel() : Parcelable {
+
+    /* var sectionSlug: String? = mSectionSlug
+     var mTitle: String? = title*/
+
+    var sectionSlug: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+    var mTitle: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    constructor(parcel: Parcel) : this() {
+        this.sectionSlug = parcel.readString()
+        this.mTitle = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(sectionSlug)
+        parcel.writeString(mTitle)
+    }
+
+
+    companion object CREATOR : Parcelable.Creator<MenuItemModel> {
+        override fun createFromParcel(parcel: Parcel): MenuItemModel {
+            return MenuItemModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MenuItemModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+/*
 class MenuItemModel : Parcelable {
 
     constructor(mSectionSlug: String?, title: String?) {
@@ -40,4 +84,4 @@ class MenuItemModel : Parcelable {
             return arrayOfNulls(size)
         }
     }
-}
+}*/
