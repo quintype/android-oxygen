045931b8f209593bcd19287e9335b83244564c65
Deepak <deepakwaitz@gmail.com>
2018-11-22 13:58:06 +0530

    MenuItemModel is extending Parcelable

--- app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
+++ app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
@@ -1,10 +1,43 @@
 package com.quintype.androidoxygen.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
  * Created TemplateCollectionWithRx by rakshith on 10/8/18.
  */
 
-class MenuItemModel(mSectionSlug: String?, title: String?) {
-    var sectionSlug: String? = mSectionSlug
-    var mTitle: String? = title
+class MenuItemModel : Parcelable {
+
+    constructor(mSectionSlug: String?, title: String?) {
+        this.sectionSlug = mSectionSlug
+        this.mTitle = title
+    }
+
+    var sectionSlug: String?
+    var mTitle: String?
+
+    constructor(parcel: Parcel) {
+        this.sectionSlug = parcel.readString()
+        this.mTitle = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(sectionSlug)
+        parcel.writeString(mTitle)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MenuItemModel> {
+        override fun createFromParcel(parcel: Parcel): MenuItemModel {
+            return MenuItemModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MenuItemModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
