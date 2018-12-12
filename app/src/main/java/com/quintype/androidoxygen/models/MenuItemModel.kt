84b05c584c81407d67b1d6c1ab86e325de7ce251
Deepak <deepakwaitz@gmail.com>
2018-12-12 10:27:06 +0530

    - Updated MenuItemModel

--- app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
+++ app/src/main/java/com/quintype/androidoxygen/models/MenuItemModel.kt
@@ -3,21 +3,23 @@ package com.quintype.androidoxygen.models
 import android.os.Parcel
 import android.os.Parcelable
 
-/**
- * Created TemplateCollectionWithRx by rakshith on 10/8/18.
- */
-
 class MenuItemModel() : Parcelable {
-
-    /* var sectionSlug: String? = mSectionSlug
-     var mTitle: String? = title*/
-
-    var sectionSlug: String? = null
+    var slug: String? = null
         get() = field
         set(value) {
             field = value
         }
-    var mTitle: String? = null
+    var title: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+    var color: Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+    var isSelected: Boolean = false
         get() = field
         set(value) {
             field = value
@@ -29,13 +31,17 @@ class MenuItemModel() : Parcelable {
     }
 
     constructor(parcel: Parcel) : this() {
-        this.sectionSlug = parcel.readString()
-        this.mTitle = parcel.readString()
+        this.slug = parcel.readString()
+        this.title = parcel.readString()
+        this.color = parcel.readInt()
+        this.isSelected = parcel.readByte().toInt() != 0
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(sectionSlug)
-        parcel.writeString(mTitle)
+        parcel.writeString(slug)
+        parcel.writeString(title)
+        parcel.writeInt(color)
+        parcel.writeByte(if (isSelected) 1.toByte() else 0.toByte())
     }
 
 
@@ -49,39 +55,3 @@ class MenuItemModel() : Parcelable {
         }
     }
 }
-
-/*
-class MenuItemModel : Parcelable {
-
-    constructor(mSectionSlug: String?, title: String?) {
-        this.sectionSlug = mSectionSlug
-        this.mTitle = title
-    }
-
-    var sectionSlug: String?
-    var mTitle: String?
-
-    constructor(parcel: Parcel) {
-        this.sectionSlug = parcel.readString()
-        this.mTitle = parcel.readString()
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(sectionSlug)
-        parcel.writeString(mTitle)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MenuItemModel> {
-        override fun createFromParcel(parcel: Parcel): MenuItemModel {
-            return MenuItemModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MenuItemModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}*/
