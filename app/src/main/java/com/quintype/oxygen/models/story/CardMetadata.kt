403d0e3b677bea80c5c6a097a219cd800d8ab96b
rakshi059 <rakshi059@gmail.com>
2020-02-08 15:57:33 +0530

    Refactored the code

--- app/src/main/java/com/quintype/oxygen/models/story/CardMetadata.kt
+++ app/src/main/java/com/quintype/oxygen/models/story/CardMetadata.kt
@@ -1,46 +1,22 @@
 package com.quintype.oxygen.models.story
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.JsonObject
-import com.google.gson.JsonParser
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
 /**
  * Created TemplateCollectionWithRx by rakshith on 9/4/18.
  */
-class CardMetadata protected constructor(`in`: Parcel) : Parcelable {
-
+@Parcelize
+data class CardMetadata protected constructor(
+    @SerializedName("social-share")
+    var socialShare: SocialShare? = null,
     @SerializedName("attributes")
-    private var attributes: JsonObject? = null
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeByte((if (attributes == null) 0 else 1).toByte())
-        if (attributes != null)
-            dest.writeString(this.attributes!!.toString())
-    }
-
-    init {
-        if (`in`.readByte().toInt() != 0) {
-            this.attributes = JsonParser().parse(`in`.readString()) as JsonObject
-        }
-    }
-
+    private var attributes: @RawValue JsonObject? = null
+) : Parcelable {
     fun attributes(): JsonObject? {
         return this.attributes
-    }
-
-    companion object CREATOR : Parcelable.Creator<CardMetadata> {
-        override fun createFromParcel(parcel: Parcel): CardMetadata {
-            return CardMetadata(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CardMetadata?> {
-            return arrayOfNulls(size)
-        }
     }
 }
